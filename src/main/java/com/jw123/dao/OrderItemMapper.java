d6505c503c222a87be56c1a788a588f99f97ddc0
jiangwei <705328475@qq.com>
2018-04-03 18:06:41 +0800

    jiangwe init project

---
+++ src/main/java/com/jw123/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.jw123.dao;
+
+import com.jw123.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
